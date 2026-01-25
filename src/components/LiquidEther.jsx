537966580de66804bd9b2bb18df1f6e7ba46461c
Jean-Marie Jung <jeanmarie.jung.pro@gmail.com>
2026-01-25 12:46:53 +0100

    LiquidEther FIx 8

--- src/components/LiquidEther.jsx
+++ src/components/LiquidEther.jsx
@@ -18,7 +18,7 @@ export default function LiquidEther({
   className = '',
   autoDemo = true,
   autoSpeed = 0.5,
-  autoIntensity = 2.2,
+  autoIntensity = 2,
   takeoverDuration = 0.25,
   autoResumeDelay = 1000,
   autoRampDuration = 0.6
